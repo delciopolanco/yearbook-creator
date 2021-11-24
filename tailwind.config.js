0dde9e463b5810d2c9cb8fd2d2b0eeb2a832868b
-.- <delciopolanco@gmail.com>
2021-11-24 16:49:33 -0400

    adding font to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        'WorkSans': ['Work_Sans', 'serif']
+      }
+    },
   },
   variants: {
     extend: {},
